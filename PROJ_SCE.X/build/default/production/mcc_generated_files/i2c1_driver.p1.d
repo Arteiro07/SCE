35c836d0ab225d8b3703648e7c7d5bdd4ec5b1c4
Fists <joaomtome1@live.com.pt>
2020-11-02 17:32:10 +0000

    Initial commit

---
+++ PROJ_SCE.X/build/default/production/mcc_generated_files/i2c1_driver.p1.d
@@ -0,0 +1,18 @@
+ build/default/production/mcc_generated_files/i2c1_driver.d  \
+ build/default/production/mcc_generated_files/i2c1_driver.p1:  \
+ mcc_generated_files/i2c1_driver.c  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/language_support.h  \
+mcc_generated_files/mcc.h  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/pic.h  \
+mcc_generated_files/device_config.h  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/proc/pic16f18875.h  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/__size_t.h  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/eeprom_routines.h  \
+mcc_generated_files/interrupt_manager.h  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/pic_chip_select.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/i2c1_driver.h  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/xc.h  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/htc.h  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/__null.h  \
+C:/Program\ Files\ (x86)/Microchip/MPLABX/v5.40/packs/Microchip/PIC16F1xxxx_DFP/1.4.119/xc8/pic/include/__at.h 
---
+++ PROJ_SCE.X/main.c
@@ -0,0 +1,302 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.65.2
+        Device            :  PIC16F18875
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+#include "I2C/i2c.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+//#include "stdio.h"
+
+/*
+                         Main application
+ */
+
+typedef struct _log{
+    uint8_t hour;      
+    uint8_t min;
+    uint8_t sec;      
+    uint8_t temp;
+    uint8_t lum;
+}log;
+
+volatile uint16_t timer_flag = 0;
+
+log reg[NREG];
+
+#define NREG 25        //number of data registers
+uint8_t PMON = 3;      //sec monitoring period
+uint8_t TALA = 5;      //sec duration of alarm signal (PWM)
+uint8_t ALAH = 12;     //hours of alarm clock
+uint8_t ALAM = 0;      //minutes of alarm clock
+uint8_t ALAS = 0;      //seconds of alarm clock
+uint8_t ALAT = 28;     //threshold for temperature alarm
+uint8_t ALAL = 4;      //threshold for luminosity level alarm
+uint8_t ALAF = 0;      //alarm flag ? starts disabled
+uint8_t CLKH = 0;      //initial value for clock hours
+uint8_t CLKM = 0;      //initial value for clock minutes
+
+
+reg[NREG];
+
+void TMR0_callback(void);
+unsigned char tsttc (void);
+
+/*
+
+#define LCD_ADDR 0x4e   // 0x27 << 1
+#define LCD_BL 0x08
+#define LCD_EN 0x04
+#define LCD_RW 0x02
+#define LCD_RS 0x01
+
+void LCDsend(unsigned char c)
+{
+  IdleI2C();
+  StartI2C(); IdleI2C();
+  WriteI2C(LCD_ADDR | 0x00); IdleI2C();
+  WriteI2C(c); IdleI2C();
+  StopI2C();    
+}
+unsigned char LCDrecv(unsigned char mode)
+{
+  unsigned char hc;
+  unsigned char lc;
+  
+  IdleI2C();
+  StartI2C(); IdleI2C();
+  WriteI2C(LCD_ADDR | 0x00); IdleI2C();
+  WriteI2C(0xf0 | LCD_BL | LCD_RW | mode); IdleI2C();
+  WriteI2C(0xf0 | LCD_BL | LCD_EN | LCD_RW | mode); IdleI2C();
+  __delay_us(1);
+  RestartI2C(); IdleI2C();
+  WriteI2C(LCD_ADDR | 0x01); IdleI2C();
+  hc = ReadI2C(); IdleI2C();
+  NotAckI2C();
+  RestartI2C(); IdleI2C();
+  WriteI2C(LCD_ADDR | 0x00); IdleI2C();
+  WriteI2C(0xf0 | LCD_BL | LCD_RW | mode); IdleI2C();
+  WriteI2C(0xf0 | LCD_BL | LCD_EN | LCD_RW | mode); IdleI2C();
+  __delay_us(1);
+  RestartI2C(); IdleI2C();
+  WriteI2C(LCD_ADDR | 0x01); IdleI2C();
+  lc = ReadI2C(); IdleI2C();
+  NotAckI2C();
+  RestartI2C(); IdleI2C();
+  WriteI2C(LCD_ADDR | 0x00); IdleI2C();
+  WriteI2C(0xf0 | LCD_BL | LCD_RW | mode); IdleI2C();
+  StopI2C();
+  return ((hc&0xf0) | ((lc>>4)&0x0f));
+}
+
+void LCDsend2x4(unsigned char c, unsigned char mode)
+{
+  unsigned char hc;
+  unsigned char lc;
+  
+  hc = c & 0xf0;
+  lc = (c << 4) & 0xf0;
+    
+  IdleI2C();
+  StartI2C(); IdleI2C();
+  WriteI2C(LCD_ADDR | 0x00); IdleI2C();
+  WriteI2C(hc | LCD_BL | mode); IdleI2C();
+  WriteI2C(hc | LCD_BL | LCD_EN | mode); IdleI2C();
+  __delay_us(1);
+  WriteI2C(hc | LCD_BL | mode); IdleI2C();
+  WriteI2C(lc | LCD_BL | mode); IdleI2C();
+  WriteI2C(lc | LCD_BL | LCD_EN | mode); IdleI2C();
+  __delay_us(1);
+  WriteI2C(lc | LCD_BL | mode); IdleI2C();
+  StopI2C();    
+  __delay_us(50);
+}
+
+void LCDinit(void)
+{
+    __delay_ms(50);
+    LCDsend(0x30);
+    LCDsend(0x34); __delay_us(500); LCDsend(0x30);
+    __delay_ms(5);
+    LCDsend(0x30);
+    LCDsend(0x34); __delay_us(500); LCDsend(0x30);
+    __delay_us(100);
+    LCDsend(0x30);
+    LCDsend(0x34); __delay_us(500); LCDsend(0x30);
+    __delay_us(100);
+    LCDsend(0x20);
+    LCDsend(0x24); __delay_us(500); LCDsend(0x20);
+    __delay_ms(5);
+
+    LCDsend2x4(0x28, 0);
+    LCDsend2x4(0x06, 0);
+    LCDsend2x4(0x0f, 0);
+    LCDsend2x4(0x01, 0);
+    __delay_ms(2);
+}
+
+void LCDcmd(unsigned char c)
+{
+  LCDsend2x4(c, 0);
+}
+
+void LCDchar(unsigned char c)
+{
+  LCDsend2x4(c, LCD_RS);
+}
+
+void LCDstr(unsigned char *p)
+{
+  unsigned char c;
+  
+  while((c = *p++)) LCDchar(c);
+}
+
+int LCDbusy()
+{
+    if(LCDrecv(0) & 0x80) return 1;
+    return 0;
+}
+
+
+ */
+ 
+void main(void)
+{
+    unsigned char c;
+    unsigned char hc;
+    unsigned char lc;
+    unsigned char c1;
+    unsigned char c2;
+    unsigned char buf[17];
+    
+    // initialize the device
+    SYSTEM_Initialize();
+    
+    // initialize timer0
+    TMR0_InterruptHandler(TMR0_callback);
+
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+    
+    i2c1_driver_open();
+    I2C_SCL = 1;
+    I2C_SDA = 1;
+    WPUC3 = 1;
+    WPUC4 = 1;
+
+    while (1)
+    {
+        // Add your application code
+        if (PMON != 0 && (timer_flag%3) == 0){
+            c = tsttc();
+            //D2_LED_Toggle();
+        }
+    }
+}
+
+void TMR0_callback(void){
+    timer_flag++;
+    if(timer_flag % 60 == 0){
+        CLKM++;
+        //send time to EEPROM
+    }
+    if(CLKM % 60 == 0){
+        CLKH++;
+        CLKM = 0;
+    }
+}
+
+void save_register(unsigned char l, unsigned char c){
+    static int n = 0;
+    log buf = (log){.hour = CLKH, .min = CLKM, .sec = (timer_flag%60), .temp = c, .lum = l};
+    reg[n] = buf;
+    n++;
+    n = n % 6;
+}
+
+unsigned char tsttc (void){
+	unsigned char value;
+do{
+	IdleI2C();
+	StartI2C(); IdleI2C();
+    
+	WriteI2C(0x9a | 0x00); IdleI2C();
+	WriteI2C(0x01); IdleI2C();
+	RestartI2C(); IdleI2C();
+	WriteI2C(0x9a | 0x01); IdleI2C();
+	value = ReadI2C(); IdleI2C();
+	NotAckI2C(); IdleI2C();
+	StopI2C();
+} while (!(value & 0x40));
+
+	IdleI2C();
+	StartI2C(); IdleI2C();
+	WriteI2C(0x9a | 0x00); IdleI2C();
+	WriteI2C(0x00); IdleI2C();
+	RestartI2C(); IdleI2C();
+	WriteI2C(0x9a | 0x01); IdleI2C();
+	value = ReadI2C(); IdleI2C();
+	NotAckI2C(); IdleI2C();
+	StopI2C();
+
+	return value;
+}
+
+
+/**
+ End of File
+*/
